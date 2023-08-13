7cf05f6edb218ea35d77e30b1f9b979897d21e1c
hasan cimcik <cimcikhasan06@gmail.com>
2023-08-13 06:32:15 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Hasan PC on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
